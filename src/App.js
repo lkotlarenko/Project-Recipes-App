47155345c8e0e68b7700f00e73bd2a3e856ec6ad
kelsonbatista <kelsonbatista@gmail.com>
2022-03-07 16:55:32 -0300

    Estrutura basica

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import ProviderApp from './context/ProviderApp';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ProviderApp>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProviderApp>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
