fce365b9a4e93f03acde6d5a7d3fdb73cdcf7fc3
kelsonbatista <kelsonbatista@gmail.com>
2022-03-08 20:50:19 -0300

    Tela de perfil

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,43 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import HeaderWithSearch from '../../components/Header/HeaderWithSearch';
 
-function Profile() {
-  localStorage.setItem('email', 'usuario@site.com');
-  const userEmail = localStorage.getItem('email');
+function Profile({ history }) {
+  const userEmail = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <HeaderWithSearch name="Profile" verific={ false } />
 
-      <div
-        className="w-11/12 h-screen mx-auto py-10 px-5 flex flex-col"
-      >
-        <p
-          data-testid="profile-email"
-        >
-          {`Email: ${userEmail}`}
+      <div className="w-11/12 h-screen mx-auto py-10 px-5 flex flex-col">
+        <p data-testid="profile-email" className="m-3">
+          {`Email: ${userEmail.email}`}
         </p>
         <div className="flex flex-col md:flex-row">
           <button
             type="button"
             className="bg-blue-500 hover:bg-blue-700 text-white
-              font-bold py-2 px-4 rounded"
+              font-bold py-2 px-4 m-3 rounded md:w-1/3"
             data-testid="profile-done-btn"
+            onClick={ () => history.push('/done-recipes') }
           >
             Done Recipes
           </button>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white
-              font-bold py-2 px-4 rounded"
+              font-bold py-2 px-4 m-3 rounded md:w-1/3"
             type="button"
             data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/favorite-recipes') }
           >
             Favorite Recipes
           </button>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white
-              font-bold py-2 px-4 rounded"
+              font-bold py-2 px-4 m-3 rounded md:w-1/3"
             type="button"
             data-testid="profile-logout-btn"
+            onClick={ () => {
+              localStorage.clear();
+              history.push('/');
+            } }
           >
             Logout
           </button>
@@ -46,5 +48,9 @@ function Profile() {
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.instanceOf(Object),
+}.isRequired;
 
 export default Profile;
