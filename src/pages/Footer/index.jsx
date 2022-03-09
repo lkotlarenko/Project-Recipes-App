19be809c000608b1e49dac3d16f5e956c4a57552
Igor Fellows <igor.fpp96@gmail.com>
2022-03-08 23:38:31 -0300

    Footer completo

---
+++ src/pages/Footer/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import exploreIcon from '../images/exploreIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+    return (
+        <Footer className="footer" data-testid="footer">
+            <Link to="/bebidas">
+                <img src={ drinkIcon } alt="drinkIcon" data-testid="drinks-bottom-btn" /> 
+            </Link>
+            <Link to="explorar">
+                <img src={ exploreIcon } alt="exploreIcon" data-testid="explore-bottom-btn"/>
+            </Link>
+            <Link to="comidas">
+                <img src={ mealIcon } alt="mealIcon" data-testid="food-bottom-btn"/>
+            </Link>
+        </Footer>
+    );
+}
+
+export default Footer;
